f121453bd6e8a12f9cd6c3cd54d665da9ec06211
kswierczek <35280799+konradswierczek@users.noreply.github.com>
2022-04-11 19:21:11 -0400

    Delete cogharm_package/build/lib/cogharm directory

--- cogharm_package/build/lib/cogharm/__init__.py
+++
@@ -1,7 +0,0 @@
-from cogharm.diatonicity import diatonicity
-from cogharm.parn88 import PitchSalience
-from cogharm.roughness import roughnessChord
-from cogharm.woolhouse09 import *
-from cogharm.ca_kon import chroma_attraction
-from cogharm.ka_kon import key_attraction
-from cogharm.corpus_analysis import Analysis
