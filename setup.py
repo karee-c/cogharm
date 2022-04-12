2ca635d47b84c61e35e0842e7390b339e7ba3b66
Konrad Swierczek <swierckj@mcmaster.ca>
2022-04-11 22:20:57 -0400

    fixing folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='cogharm',
+version='0.1',
+description='tools for music analysis based on psychology',
+url='#',
+author='Konrad Swierczek, Karen Chan, Matthew Woolhouse',
+install_requires=['opencv-python'],
+author_email='swierckj@mcmaster.ca',
+packages=setuptools.find_packages(),
+zip_safe=False)
